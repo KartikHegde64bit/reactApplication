ac2ed778c99b57ffc622656bf27723240e3f6ac0
Kartik Hegde <kartikhegde062@gmail.com>
2023-12-25 15:43:33 +0530

    inital commit

---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+// PascalCasing is used to name function components
+function Message(){
+    // JSX - Javascript XMLformat where HTML code is returned in js file.
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
